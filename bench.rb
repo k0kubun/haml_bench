8d5575dd084f84da5b52df75c0b94dc54beafc36
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-07 17:02:29 +0900

    Simulate Rails behavior for https://github.com/haml/haml/pull/897

--- bench.rb
+++ bench.rb
@@ -20,8 +20,12 @@ Benchmark.ips do |x|
   haml_code = File.read("#{__dir__}/templates/view.haml")
   context = Context.new
 
+  buffer_options = { ugly: true, escape_html: true }
+  if Haml::Options.respond_to?(:buffer_defaults)
+    Haml::Options.buffer_defaults.merge!(buffer_options)
+  end
+  Haml::Engine.new(haml_code, buffer_options.merge(format: :html5)).def_method(context, :run_haml)
   Haml4::Engine.new(haml_code, format: :html5, ugly: true, escape_html: true).def_method(context, :run_haml4)
-  Haml::Engine.new(haml_code, format: :html5, ugly: true, escape_html: true).def_method(context, :run_haml)
   context.instance_eval %Q[
     def run_faml; #{Faml::Engine.new.call(haml_code)}; end
     def run_hamlit; #{Hamlit::Engine.new.call(haml_code)}; end

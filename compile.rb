93194cc73e1f41221f1e0515d9daef6a7f740a71
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-07 17:44:11 +0900

    Show Hamlit

--- compile.rb
+++ compile.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require 'haml'
+require 'hamlit'
 
 haml_code = File.read("#{__dir__}/#{ARGV.first || 'templates/slim_bench.haml'}")
 
@@ -8,5 +9,10 @@ if Haml::Options.respond_to?(:buffer_defaults)
   Haml::Options.buffer_defaults.merge!(buffer_options)
 end
 haml_src = Haml::Engine.new(haml_code, buffer_options.merge(format: :html5)).compiler.precompiled_with_ambles([])
+hamlit_src = Hamlit::Engine.new.call(haml_code)
 
+puts "=============================[hamlit]==================================="
+puts Pry.Code(hamlit_src).highlighted
+puts
+puts "=============================[haml]====================================="
 puts Pry.Code(haml_src).highlighted
